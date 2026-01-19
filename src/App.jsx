cfa8316037168d75724d0a5da7b7912df941be6d
suhanibansal870 <83600598+suhanibansal870@users.noreply.github.com>
2026-01-19 15:00:58 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useMemo, useState } from "react";
+const [open, setOpen] = useState(false);
+export default function App() {
+  return <div>hello</div>;
+}
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// CSS (from src/styles/)
+import "./styles/bootstrap.min.css";
+import "./styles/font-awesome.min.css";
+import "./styles/flaticon.css";
+import "./styles/animate.css";
+import "./styles/owl.carousel.min.css";
+import "./styles/owl.theme.default.min.css";
+import "./styles/bootsnav.css";
+import "./styles/style.css";
+import "./styles/responsive.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
